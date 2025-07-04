f51fb7fd826ee06f6ac8cf7433871a52b3c8aaee
Verve-Kalpa <sw5@verveautomation.com>
2025-07-04 10:17:20 +0530

    Refactor main layout and update home page redirect
    Moved the main layout structure to a new Pagelayout component in (main)/layout.tsx. Deleted the redundant (main)/page.tsx and updated the root page to redirect to /login instead of rendering the dashboard layout.

---
+++ src/app/(main)/layout.tsx
@@ -0,0 +1,20 @@
+import SideBar from "@/components/layout/sideBar";
+import TopBar from "@/components/layout/topBar";
+import React from "react";
+
+const Pagelayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex h-screen overflow-hidden">
+      <SideBar />
+      {/* Main content area - Only needs margin on mobile/tablet */}
+      <div className="flex-1 bg-[var(--background)] flex flex-col overflow-hidden ml-16 sm:ml-20 lg:ml-0">
+        {/* Top Bar */}
+        <TopBar />
+        {/* Main content */}
+        <div className="p-4 sm:p-12 flex-1 overflow-auto">{children}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Pagelayout;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
-import TopBar from "@/components/layout/topBar";
-import SideBar from "@/components/layout/sideBar";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return <div className="min-h-screen bg-white"></div>;
+  redirect("/login");
+  return;
 }
