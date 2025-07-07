3c8a61be92d9dd5ee9e886980c03f1f7252053db
chamodkamiss-Verve <sw4@verveautomation.com>
2025-07-07 14:14:44 +0530

    Revert "Merge branch 'dev' of https://github.com/VerveAutomation-SL/M_RES_FE into dev"
    This reverts commit 57981c5fcde3b819d5c0e5b8fdc4dff9b448cd6a, reversing
    changes made to e551da0068b99f4d927f2ac390cd8a717ff78a62.

--- src/app/(main)/test/page.tsx
+++ src/app/(main)/test/page.tsx
@@ -1,6 +1,5 @@
-
-import RoomGrid from "@/components/layout/roomGrid";
-import Modal from "@/components/ui/modal";
+import ResortForm from "@/components/forms/resortForm";
+import RoomForm from "@/components/forms/roomForm";
 import React from "react";
 
 const Page = () => {
@@ -8,7 +7,8 @@ const Page = () => {
     <>
       <div className="h-full bg-gray-200">
         {/* <div>Test Page</div> */}
-        <RoomGrid/>
+        <RoomForm />
+        <ResortForm/>
       </div>
     </>
   );
