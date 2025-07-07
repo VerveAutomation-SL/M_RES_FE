dcba4633edb4eb4d4c403e631b722561ae373510
Verve-Kalpa <sw5@verveautomation.com>
2025-07-07 13:39:08 +0530

    Update header styles for improved appearance
    Adjusted padding, font weights, and colors in the Header component for better visual hierarchy and consistency. Enhanced the add button with new styles, responsive sizing, and hover effects.

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -11,20 +11,22 @@ interface HeaderProps {
 const Header = ({ title, subtitle, addButton, onClick }: HeaderProps) => {
   return (
     <>
-      <div className="flex items-center justify-between md:px-5 md:py-2">
+      <div className="flex items-start justify-between px-2 md:px-5 md:py-2">
         <div>
-          <h1 className="text-2xl lg:text-4xl font-semibold text-gray-900">
+          <h1 className="text-2xl lg:text-4xl text-[var(--primary)] font-extrabold">
             {title}
           </h1>
-          <p className="text-sm lg:text-lg text-gray-600 mt-1">{subtitle}</p>
+          <p className="text-sm lg:text-lg text-[var(--secondary)] mt-1">
+            {subtitle}
+          </p>
         </div>
         {addButton && (
           <button
-            className="flex items-center p-2 text-[var(--primary)] border-2 rounded-full"
+            className="flex items-center p-1 lg:p-2 text-xs text-[var(--primary)] border-4 rounded-full hover:bg-[var(--primary)] hover:text-white transition-all duration-200"
             onClick={onClick}
           >
-            <Plus className="w-5 h-5 mr-2" />
-            {addButton}
+            <Plus className="w-3 h-3 lg:w-5 lg:h-5 mr-2" />
+            <span className="items-center">{addButton}</span>
           </button>
         )}
       </div>
